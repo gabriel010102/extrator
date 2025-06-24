f85b74633cd2b86180a2fef94f7f25a95fe82fa4
gabriel010102 <gabriel.rocha24@escola.pr.gov.br>
2025-06-24 08:35:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavra-chave');
+
+botaoMostraPalavras.addEventListener('click',mostraPalavrasChave);
+
+function mostraPalavrasChave(){
+   alert('fui clicado!');
+}
